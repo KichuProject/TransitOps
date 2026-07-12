e0bd24296c61d11114769f2057d724656edc21c2
KichuProject <kishorekichuproj@gmail.com>
2026-07-12 10:42:15 +0530

    inital commit

---
+++ backend/src/main/java/com/example/transitops/transit/TransitApplication.java
@@ -0,0 +1,13 @@
+package com.example.transitops.transit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitApplication.class, args);
+	}
+
+}
